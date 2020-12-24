de894077cdb7b34104a8f93a4f596b96fcc88709
Navod Zoysa <55632254+NavodZoysa@users.noreply.github.com>
2020-12-25 00:21:05 +0530

    initialized ui

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -1,35 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { AppService } from './app.service';
+
+import { of } from 'rxjs';
+
+class FakeAppService extends AppService {
+  getWelcomeMessage() {
+    return of({
+      content: 'Test content'
+    });
+  }
+}
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        HttpClientModule,
+        RouterTestingModule
+      ]
+    }).overrideComponent(AppComponent, {
+      set: {
+        providers: [
+          { provide: AppService, useClass: FakeAppService}
+        ]
+      }
     }).compileComponents();
-  });
-
-  it('should create the app', () => {
+  }));
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ui'`, () => {
+  }));
+  it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ui');
-  });
-
-  it('should render title', () => {
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Test content');
+  }));
+  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ui app is running!');
-  });
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Test content!');
+  }));
 });
