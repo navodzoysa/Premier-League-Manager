dc14f9fad32052affd8c277fcc214dfad19257e1
Navod Zoysa <55632254+NavodZoysa@users.noreply.github.com>
2020-12-24 22:47:56 +0530

    removed files

--- project/FrontendCommands.scala
+++
@@ -1,6 +0,0 @@
-object FrontendCommands {
-  val dependencyInstall: String = "npm install"
-  val test: String = "npm run test:ci"
-  val serve: String = "npm run start"
-  val build: String = "npm run build:prod"
-}
