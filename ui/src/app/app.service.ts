fcdd91f53edaae7b4b6293987fb71b6a91febaa4
Navod Zoysa <55632254+NavodZoysa@users.noreply.github.com>
2021-01-03 18:24:02 +0530

    Refactored unused code

--- ui/src/app/app.service.ts
+++ ui/src/app/app.service.ts
@@ -26,15 +26,6 @@ export class AppService {
   constructor(private http: HttpClient) {
   }
 
-  /**
-   * Makes a http get request to retrieve the welcome message from the backend service.
-   */
-  public getWelcomeMessage() {
-    return this.http.get(this.serviceUrl).pipe(
-      map(response => response)
-    );
-  }
-
   // HTTP request to get all clubs from backend
   public getClubs() {
     return this.http.get(this.clubsUrl);
@@ -74,12 +65,5 @@ export class AppService {
 
   public searchMatch(date: string) {
     return this.http.post(this.filterMatchesUrl, {date});
-  }
-
-  /**
-   * Makes a http post request to send some data to backend & get response.
-   */
-  public sendData(): Observable<any> {
-    return this.http.post(this.dataPostTestUrl, {});
   }
 }
