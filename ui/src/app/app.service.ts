bd7e8f725d28d58a76842833d4b719c8785a3a06
Navod Zoysa <55632254+NavodZoysa@users.noreply.github.com>
2021-01-04 07:27:33 +0530

    Gave comments to http requests

--- ui/src/app/app.service.ts
+++ ui/src/app/app.service.ts
@@ -21,36 +21,40 @@ export class AppService {
   private filterMatchesUrl = '/api/filtermatches';
   private tableEvent: Subject<any> = new Subject<any>();
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   // HTTP request to get all clubs from backend
   public getClubs() {
     return this.http.get(this.clubsUrl);
   }
-
+  // HTTP request to sort the clubs and get all clubs from backend
   public sortPoints() {
     return this.http.get(this.sortPointsUrl);
   }
-
+  // HTTP request to sort the clubs and get all clubs from backend
   public sortGoals() {
     return this.http.get(this.sortGoalsUrl);
   }
-
+  // HTTP request to sort the clubs and get all clubs from backend
   public sortWins() {
     return this.http.get(this.sortWinsUrl);
   }
-
+  // HTTP request to get all matches from backend
   public getMatches() {
     return this.http.get(this.matchesUrl);
   }
-
+  // HTTP request to sort the matches and get all matches from backend
   public sortMatches() {
     return this.http.get(this.sortMatchesUrl);
   }
-
+  // HTTP request to add a random match then send back the added match
   public postRandomMatch(): Observable<any> {
     return this.http.post(this.matchUrl, {});
+  }
+  // HTTP request to sent a date to the backend and retrieve a filtered list of
+  // matches for that date
+  public searchMatch(date: string) {
+    return this.http.post(this.filterMatchesUrl, {date});
   }
 
   public listenTableEvent() {
@@ -59,9 +63,5 @@ export class AppService {
 
   public tableEventObserver(value: any) {
     this.tableEvent.next(value);
-  }
-
-  public searchMatch(date: string) {
-    return this.http.post(this.filterMatchesUrl, {date});
   }
 }
